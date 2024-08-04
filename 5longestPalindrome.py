54cd4e80a6b0263ca3ce72a4b1f5df0fd0b017d2
Valentino <69142225+SniIld@users.noreply.github.com>
2024-08-04 15:10:40 +0300

    Create 5longestPalindrome.py

---
+++ 5longestPalindrome.py
@@ -0,0 +1,13 @@
+def longestPalindrome(self, s: str) -> str:
+  resultList = []
+  resultList.append(s[0])
+  
+  for startIndex, i in enumerate(s):
+    for endIndex, j in enumerate(s):
+      if i == j and startIndex != endIndex:
+        a = s[startIndex: endIndex + 1]
+        
+        if a == a[::-1]:
+          resultList.append(a)
+          
+  return max(resultList, key=len)
