dfebdb99300acbb7af5c78a6c3dc04d68bbf3c35
Valentino <69142225+SniIld@users.noreply.github.com>
2024-08-06 02:47:37 +0300

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,10 @@
+// for1.rs
+fn main() {
+    let mut result = 0;
+    for i in 0..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            result += i;
+        }
+    }
+    println!("{result}")
+}
