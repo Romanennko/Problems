32605970894e3bd06f85c7977b1a67081064face
Valentino <69142225+SniIld@users.noreply.github.com>
2024-08-11 00:55:30 +0300

    Add files via upload

---
+++ LeetCode/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+        commonFactors = []
+        i = 1
+        while a >= i or b >= i:
+            if a % i == 0 and b % i == 0:
+                commonFactors.append(i)
+            i += 1
+
+        return len(commonFactors)
+
+
+asd = Solution()
+print(asd.commonFactors(885, 885))
