87c15013172c24dd9e4e5540169afa046f0ad9f5
Mykyta <69142225+Romanennko@users.noreply.github.com>
2024-12-16 17:52:12 +0100

    The program, which will take the url from the user, will write it out on the screen, and on a separate line will provide the protocol identifier from the url, if any.

---
+++ University/Programming-Basics-1/Lab6/Task6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void check_protocol(char *url, char *protocol);
+
+
+int main() {
+    char url[256], protocol[50];
+
+    printf("Enter the URL: ");
+    scanf("%s", url);
+    while(getchar() != '\n');
+
+    check_protocol(url, protocol);
+
+    printf("\nProtocol: %s\n", protocol);
+
+    return 0;
+}
+
+
+void check_protocol(char *url, char *protocol) {
+    int count = 0;
+
+    for (int i = 0; i < strlen(url) - 2; i++) {
+        if (url[i] == ':' && url[i + 1] == '/' && url[i + 2] == '/') {
+            strncpy(protocol, url, i);
+            break;
+        }
+    }
+}
