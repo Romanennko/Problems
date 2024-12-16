b8378be34a8096d4c0b9055465bccb884ebf9948
Mykyta <69142225+Romanennko@users.noreply.github.com>
2024-12-16 17:45:21 +0100

    Program that will check whether the two words given by the user are anagrams.

---
+++ University/Programming-Basics-1/Lab6/Task2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void sorted_word(char *word, int len);
+void its_anagram(char *fw, char *sw, int len);
+
+
+int main()
+{
+    char first_word[50], second_word[50];
+
+    printf("Enter the first word: ");
+    scanf("%s", first_word);
+    while (getchar() != '\n');
+
+    printf("Enter the second word: ");
+    scanf("%s", second_word);
+    while (getchar() != '\n');
+
+    if (strlen(first_word) != strlen(second_word))
+        puts("These words are not anagrams");
+    else {
+        sorted_word(first_word, strlen(first_word));
+        sorted_word(second_word, strlen(second_word));
+
+        its_anagram(first_word, second_word, strlen(first_word));
+    }
+    return 0;
+}
+
+
+void sorted_word(char *word, int len) {
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (tolower(word[i]) < tolower(word[j])) {
+                char temp = tolower(word[i]);
+                word[i] = tolower(word[j]);
+                word[j] = temp;
+            }
+        }
+    }
+}
+
+
+void its_anagram(char *fw, char *sw, int len) {
+    for (int i = 0; i < len; i++) {
+        if (fw[i] != sw[i]) {
+            puts("\nThese words are not anagrams");
+            return;
+        }
+    }
+
+    puts("\nThese words are anagrams");
+}
