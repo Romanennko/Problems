637d2f753ac358887b02b917c820e36490bda8eb
Mykyta <69142225+Romanennko@users.noreply.github.com>
2025-01-27 11:05:23 +0100

    A program that finds the largest and smallest value in an int-type number matrix of size 3×4. Fill the matrix with integers drawn at random from -5 to 5 and print its contents on the screen.

--- University/Programming-Basics-1/Lab7/Task2.c
+++ University/Programming-Basics-1/Lab7/Task2.c
@@ -2,76 +2,66 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MIN_RANDOM_NUMBER -5
+#define MAX_RANDOM_NUMBER 5
+
 #define ROWS 3
-#define COLUMNS 4
-#define RANDOM_MAX 5
+#define COLS 4
 
-void fill_matrix(int matrix[ROWS][COLUMNS]);
-void print_matrix(int matrix[ROWS][COLUMNS]);
-int find_min_in_matrix(int matrix[ROWS][COLUMNS]);
-int find_max_in_matrix(int matrix[ROWS][COLUMNS]);
+void fill_matrix_random_numbers(int matrix[ROWS][COLS]);
+void find_min_and_max_elements(int matrix[ROWS][COLS], int *min, int *max);
+void print_matrix(int matrix[ROWS][COLS]);
 
-int main()
-{
-    int matrixA[ROWS][COLUMNS];
+int main() {
+    int matrix[ROWS][COLS];
 
-    fill_matrix(matrixA);
+    fill_matrix_random_numbers(matrix);
+    
+    int min, max;
 
-    printf("Matrix A:\n");
-    print_matrix(matrixA);
-
-    printf("\nMinimum value = %d", find_min_in_matrix(matrixA));
-    printf("\nMaximum value = %d\n", find_max_in_matrix(matrixA));
+    find_min_and_max_elements(matrix, &min, &max);
+    print_matrix(matrix);
+    printf("Min: %d, Max: %d\n", min, max);
 
     return 0;
 }
 
 
-void fill_matrix(int matrix[ROWS][COLUMNS])
-{
-    srand(time(0));
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLUMNS; j++) {
-            matrix[i][j] = -5 + rand()%11;
+void fill_matrix_random_numbers(int matrix[ROWS][COLS]) {
+    int i, j;
+    srand(time(NULL));
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            matrix[i][j] = MIN_RANDOM_NUMBER + rand() % (MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER + 1);
         }
     }
 }
 
 
-void print_matrix(int matrix[ROWS][COLUMNS])
-{
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLUMNS; j++) {
-            printf("%4d", matrix[i][j]);
+void find_min_and_max_elements(int matrix[ROWS][COLS], int *min, int *max) {
+    *min = matrix[0][0];
+    *max = matrix[0][0];
+
+    int i, j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            if (matrix[i][j] < *min) {
+                *min = matrix[i][j];
+            }
+            if (matrix[i][j] > *max) {
+                *max = matrix[i][j];
+            }
+        }
+    }
+}
+
+
+void print_matrix(int matrix[ROWS][COLS]) {
+    int i, j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            printf("%3d ", matrix[i][j]);
         }
         printf("\n");
     }
-}
-
-
-int find_min_in_matrix(int matrix[ROWS][COLUMNS])
-{
-    int min = matrix[0][0];
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLUMNS; j++) {
-            if (matrix[i][j] < min) {
-                min = matrix[i][j];
-            }
-        }
-    }
-    return min;
-}
-
-
-int find_max_in_matrix(int matrix[ROWS][COLUMNS])
-{
-    int max = matrix[0][0];
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLUMNS; j++) {
-            if (matrix[i][j] > max) {
-                max = matrix[i][j];
-            }
-        }
-    }
-    return max;
 }
